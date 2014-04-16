09011bb8bd299e4b3e63bccd534e51bf4c9c7b23
Joan Romano <joanromano@gmail.com>
2014-04-16 19:08:36 +0200

    Update API documentation

--- Source/CADRACSwippeableCell.h
+++ Source/CADRACSwippeableCell.h
@@ -17,11 +17,33 @@ typedef NS_ENUM(NSUInteger, CADRACSwippeableCellAllowedDirection) {
 
 @interface CADRACSwippeableCell : UICollectionViewCell
 
+/** 
+ Signal which sends @(YES) values each time the reveal view is shown and @(NO) values when it's hidden.
+ */
 @property (nonatomic, strong, readonly) RACSignal *revealViewSignal;
+
+/**
+ The view beneath the cell that will be shown. You should provide this view if you want your cell to be swippeable.
+ */
 @property (nonatomic, strong) UIView *revealView;
+
+/**
+ The allowed swipe direction. Defaults to CADRACSwippeableCellAllowedDirectionLeft.
+ */
 @property (nonatomic) CADRACSwippeableCellAllowedDirection allowedDirection;
 
+/**
+ Explicitly hide the reveal view.
+ 
+ @param animated YES if we want the action to be animated, NO otherwise
+ */
 - (void)hideRevealViewAnimated:(BOOL)animated;
+
+/**
+ Explicitly show the reveal view.
+ 
+ @param animated YES if we want the action to be animated, NO otherwise
+ */
 - (void)showRevealViewAnimated:(BOOL)animated;
 
 @end
